3ad00573cc2bef8bddd29f888b8f5541e38779e2
Matthew Sandoval <mat24c@gmail.com>
2020-05-04 10:31:28 -0700

    added uri for local test

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(cors({
 app.use(cookieParser())
 app.use(express.json())
 
-// const uri = config.get('mongoURI')
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
+const uri = config.get('mongoURI')
+mongoose.connect(process.env.MONGODB_URI || uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
 const connection = mongoose.connection
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully")
